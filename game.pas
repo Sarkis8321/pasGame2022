7b409db1be526510fbb877ad09992ccd09f93a00
Sarkis8321 <danielyan.sar@yandex.ru>
2022-10-24 16:11:03 +0400

    30

--- game.pas
+++ game.pas
@@ -1,6 +1,6 @@
 ﻿uses GraphWPF, WPFObjects;
 
-var ballRadius := 20;
+var ballRadius := 30;
 
 var Ball := new CircleWPF(ballRadius,ballRadius,ballRadius,gcolor.FromRgb(255,0,0));
 var Enemy := new CircleWPF(810-ballRadius,610-ballRadius,ballRadius,gcolor.FromRgb(0,0,255));
